83d199debcd6146bb3e4c78bf9d7d0dd21913a77
lucasmsouza24 <lucas.msouza@bandtec.com.br>
2021-11-20 20:34:30 -0300

    db update

--- database/create.sql
+++ database/create.sql
@@ -5,17 +5,16 @@ CREATE DATABASE ac3;
 
 USE ac3;
 
-CREATE TABLE tecnologias(
+CREATE TABLE person(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(20)
+    name VARCHAR(20) NOT NULL,
+    age INT NOT NULL
 );
 
 -- DML
-INSERT INTO tecnologias(name) VALUES 
-('node'),
-('javascript'),
-('java'),
-('python'),
-('docker'),
-('mysql'),
-('mssql')
+INSERT INTO person(name, age) VALUES 
+('lucas mesquita', 20),
+('jonh mayer', 37),
+('ringo starr', 50),
+('dan reynalds', 28),
+('gal gadot', 18)
